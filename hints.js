233284435f2e544b78b98ebb9307dc6e8225b542
KmarsHub <53037997+KmarsHub@users.noreply.github.com>
2019-08-17 07:16:59 +0500

    Update hints.js

--- hints.js
+++ hints.js
@@ -450,7 +450,7 @@
     completeSingle: true,
     alignWithWord: true,
     closeCharacters: /[\s()\[\]{};:>,]/,
-    closeOnUnfocus: true,
+    closeOnUnfocus: false, /* default is true*/
     completeOnSingleClick: true,
     container: null,
     customKeys: null,
@@ -815,7 +815,7 @@
     samp: s,
     script: {
       attrs: {
-        type: ["text/javascript"],
+        type: d+json", "text/ecmascript", "application/javascript", "application/x-javascript", "application/ecmascript", "application/x-json", "text/typescript", "application/typescript"],
         src: null,
         async: ["", "async"],
         defer: ["", "defer"],
@@ -846,6 +846,7 @@
     sub: s,
     summary: s,
     sup: s,
+    svg: s,
     table: s,
     tbody: s,
     td: { attrs: { colspan: null, rowspan: null, headers: null } },
@@ -893,14 +894,14 @@
 
   var globalAttrs = {
     accesskey: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    "class": null,
+    "class": ["kmas", "kmars", "kmarshub", "KH", "kh", "k-h", "KmarsHub", "Kmars-Hub", "kmars-hub", "k-mars-hub", "k-marshub", "K-mars-hub", "K-marshub", "K-Mars-Hub", "K-MarsHub", "K-mars-Hub", "K-marsHub"],
     contenteditable: ["true", "false"],
     contextmenu: null,
     dir: ["ltr", "rtl", "auto"],
     draggable: ["true", "false", "auto"],
     dropzone: ["copy", "move", "link", "string:", "file:"],
     hidden: ["hidden"],
-    id: null,
+    id: ["kmas", "kmars", "kmarshub", "KH", "kh", "k-h", "KmarsHub", "Kmars-Hub", "kmars-hub", "k-mars-hub", "k-marshub", "K-mars-hub", "K-marshub", "K-Mars-Hub", "K-MarsHub", "K-mars-Hub", "K-marsHub"],
     inert: ["inert"],
     itemid: null,
     itemprop: null,
@@ -911,9 +912,9 @@
     spellcheck: ["true", "false"],
     autocorrect: ["true", "false"],
     autocapitalize: ["true", "false"],
-    style: null,
+    style: ["align-content:; " , "align-items:; " , "align-self:; " , "alignment-adjust:; " , "alignment-baseline:; " , "anchor-point:; " , "animation:; " , "animation-delay:; " , "animation-direction:; " , "animation-duration:; " , "animation-fill-mode:; " , "animation-iteration-count:; " , "animation-name:; " , "animation-play-state:; " , "animation-timing-function:; " , "appearance:; " , "azimuth:; " , "backface-visibility:; " , "background:; " , "background-attachment:; " , "background-blend-mode:; " , "background-clip:; " , "background-color:; " , "background-image:; " , "background-origin:; " , "background-position:; " , "background-repeat:; " , "background-size:; " , "baseline-shift:; " , "binding:; " , "bleed:; " , "bookmark-label:; " , "bookmark-level:; " , "bookmark-state:; " , "bookmark-target:; " , "border:; " , "border-bottom:; " , "border-bottom-color:; " , "border-bottom-left-radius:; " , "border-bottom-right-radius:; " , "border-bottom-style:; " , "border-bottom-width:; " , "border-collapse:; " , "border-color:; " , "border-image:; " , "border-image-outset:; " , "border-image-repeat:; " , "border-image-slice:; " , "border-image-source:; " , "border-image-width:; " , "border-left:; " , "border-left-color:; " , "border-left-style:; " , "border-left-width:; " , "border-radius:; " , "border-right:; " , "border-right-color:; " , "border-right-style:; " , "border-right-width:; " , "border-spacing:; " , "border-style:; " , "border-top:; " , "border-top-color:; " , "border-top-left-radius:; " , "border-top-right-radius:; " , "border-top-style:; " , "border-top-width:; " , "border-width:; " , "bottom:; " , "box-decoration-break:; " , "box-shadow:; " , "box-sizing:; " , "break-after:; " , "break-before:; " , "break-inside:; " , "caption-side:; " , "caret-color:; " , "clear:; " , "clip:; " , "color:; " , "color-profile:; " , "column-count:; " , "column-fill:; " , "column-gap:; " , "column-rule:; " , "column-rule-color:; " , "column-rule-style:; " , "column-rule-width:; " , "column-span:; " , "column-width:; " , "columns:; " , "content:; " , "counter-increment:; " , "counter-reset:; " , "crop:; " , "cue:; " , "cue-after:; " , "cue-before:; " , "cursor:; " , "direction:; " , "display:; " , "dominant-baseline:; " , "drop-initial-after-adjust:; " , "drop-initial-after-align:; " , "drop-initial-before-adjust:; " , "drop-initial-before-align:; " , "drop-initial-size:; " , "drop-initial-value:; " , "elevation:; " , "empty-cells:; " , "fit:; " , "fit-position:; " , "flex:; " , "flex-basis:; " , "flex-direction:; " , "flex-flow:; " , "flex-grow:; " , "flex-shrink:; " , "flex-wrap:; " , "float:; " , "float-offset:; " , "flow-from:; " , "flow-into:; " , "font:; " , "font-feature-settings:; " , "font-family:; " , "font-kerning:; " , "font-language-override:; " , "font-size:; " , "font-size-adjust:; " , "font-stretch:; " , "font-style:; " , "font-synthesis:; " , "font-variant:; " , "font-variant-alternates:; " , "font-variant-caps:; " , "font-variant-east-asian:; " , "font-variant-ligatures:; " , "font-variant-numeric:; " , "font-variant-position:; " , "font-weight:; " , "grid:; " , "grid-area:; " , "grid-auto-columns:; " , "grid-auto-flow:; " , "grid-auto-rows:; " , "grid-column:; " , "grid-column-end:; " , "grid-column-gap:; " , "grid-column-start:; " , "grid-gap:; " , "grid-row:; " , "grid-row-end:; " , "grid-row-gap:; " , "grid-row-start:; " , "grid-template:; " , "grid-template-areas:; " , "grid-template-columns:; " , "grid-template-rows:; " , "hanging-punctuation:; " , "height:; " , "hyphens:; " , "icon:; " , "image-orientation:; " , "image-rendering:; " , "image-resolution:; " , "inline-box-align:; " , "justify-content:; " , "justify-items:; " , "justify-self:; " , "left:; " , "letter-spacing:; " , "line-break:; " , "line-height:; " , "line-stacking:; " , "line-stacking-ruby:; " , "line-stacking-shift:; " , "line-stacking-strategy:; " , "list-style:; " , "list-style-image:; " , "list-style-position:; " , "list-style-type:; " , "margin:; " , "margin-bottom:; " , "margin-left:; " , "margin-right:; " , "margin-top:; " , "marks:; " , "marquee-direction:; " , "marquee-loop:; " , "marquee-play-count:; " , "marquee-speed:; " , "marquee-style:; " , "max-height:; " , "max-width:; " , "min-height:; " , "min-width:; " , "mix-blend-mode:; " , "move-to:; " , "nav-down:; " , "nav-index:; " , "nav-left:; " , "nav-right:; " , "nav-up:; " , "object-fit:; " , "object-position:; " , "opacity:; " , "order:; " , "orphans:; " , "outline:; " , "outline-color:; " , "outline-offset:; " , "outline-style:; " , "outline-width:; " , "overflow:; " , "overflow-style:; " , "overflow-wrap:; " , "overflow-x:; " , "overflow-y:; " , "padding:; " , "padding-bottom:; " , "padding-left:; " , "padding-right:; " , "padding-top:; " , "page:; " , "page-break-after:; " , "page-break-before:; " , "page-break-inside:; " , "page-policy:; " , "pause:; " , "pause-after:; " , "pause-before:; " , "perspective:; " , "perspective-origin:; " , "pitch:; " , "pitch-range:; " , "place-content:; " , "place-items:; " , "place-self:; " , "play-during:; " , "position:; " , "presentation-level:; " , "punctuation-trim:; " , "quotes:; " , "region-break-after:; " , "region-break-before:; " , "region-break-inside:; " , "region-fragment:; " , "rendering-intent:; " , "resize:; " , "rest:; " , "rest-after:; " , "rest-before:; " , "richness:; " , "right:; " , "rotation:; " , "rotation-point:; " , "ruby-align:; " , "ruby-overhang:; " , "ruby-position:; " , "ruby-span:; " , "shape-image-threshold:; " , "shape-inside:; " , "shape-margin:; " , "shape-outside:; " , "size:; " , "speak:; " , "speak-as:; " , "speak-header:; " , "speak-numeral:; " , "speak-punctuation:; " , "speech-rate:; " , "stress:; " , "string-set:; " , "tab-size:; " , "table-layout:; " , "target:; " , "target-name:; " , "target-new:; " , "target-position:; " , "text-align:; " , "text-align-last:; " , "text-decoration:; " , "text-decoration-color:; " , "text-decoration-line:; " , "text-decoration-skip:; " , "text-decoration-style:; " , "text-emphasis:; " , "text-emphasis-color:; " , "text-emphasis-position:; " , "text-emphasis-style:; " , "text-height:; " , "text-indent:; " , "text-justify:; " , "text-outline:; " , "text-overflow:; " , "text-shadow:; " , "text-size-adjust:; " , "text-space-collapse:; " , "text-transform:; " , "text-underline-position:; " , "text-wrap:; " , "top:; " , "transform:; " , "transform-origin:; " , "transform-style:; " , "transition:; " , "transition-delay:; " , "transition-duration:; " , "transition-property:; " , "transition-timing-function:; " , "unicode-bidi:; " , "user-select:; " , "vertical-align:; " , "visibility:; " , "voice-balance:; " , "voice-duration:; " , "voice-family:; " , "voice-pitch:; " , "voice-range:; " , "voice-rate:; " , "voice-stress:; " , "voice-volume:; " , "volume:; " , "white-space:; " , "widows:; " , "width:; " , "will-change:; " , "word-break:; " , "word-spacing:; " , "word-wrap:; " , "z-index:; " , "clip-path:; " , "clip-rule:; " , "mask:; " , "enable-background:; " , "filter:; " , "flood-color:; " , "flood-opacity:; " , "lighting-color:; " , "stop-color:; " , "stop-opacity:; " , "pointer-events:; " , "color-interpolation:; " , "color-interpolation-filters:; " , "color-rendering:; " , "fill:; " , "fill-opacity:; " , "fill-rule:; " , "marker:; " , "marker-end:; " , "marker-mid:; " , "marker-start:; " , "shape-rendering:; " , "stroke:; " , "stroke-dasharray:; " , "stroke-dashoffset:; " , "stroke-linecap:; " , "stroke-linejoin:; " , "stroke-miterlimit:; " , "stroke-opacity:; " , "stroke-width:; " , "text-rendering:; " , "glyph-orientation-horizontal:; " , "glyph-orientation-vertical:; " , "text-anchor:; " , "writing-mode:; "],
     tabindex: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    title: null,
+    title: ["kmas", "kmars", "kmarshub", "KH", "kh", "k-h", "KmarsHub", "Kmars-Hub", "kmars-hub", "k-mars-hub", "k-marshub", "K-mars-hub", "K-marshub", "K-Mars-Hub", "K-MarsHub", "K-mars-Hub", "K-marsHub"],
     translate: ["yes", "no"],
     onclick: null,
     rel: ["stylesheet", "alternate", "author", "bookmark", "help", "license", "next", "nofollow", "noreferrer", "prefetch", "prev", "search", "tag"]
